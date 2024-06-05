c4c50997cca178d9675f8c031686490c3a955331
zs53 <eval@etidat.com>
2024-06-05 21:14:53 +0200

    upd

--- src/models/carga.model.js
+++ src/models/carga.model.js
@@ -68,6 +68,7 @@ export const insert = async (context) => {
   // proc
   try {
     const result = await simpleExecute(insertSql, bind)
+    console.log('result...',result);
   
     if (result) {
       bind.IDCARG = result.outBinds.IDCARG
